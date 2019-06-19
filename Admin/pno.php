0ce78620d1e8ec44c34568afabfd0a72be2ec3ae
RajeshPandey057 <rajeshpandey1998@yahoo.com>
2019-06-19 19:27:37 +0530

    Added Some pages
    New Buttons, and pages added

--- Admin/pno.php
+++ Admin/pno.php
@@ -1,13 +1,13 @@
 <?php
 include 'connect.php';
 
- $qry="SELECT ID FROM storie_no";
+ $qry="SELECT sno FROM storie_no";
  
- if($result=mysqli_query($conn,$qry))
- {
-    $pno=mysqli_num_rows($result);
-    $pno=$pno+1;
-    $sql="INSERT INTO storie_no(sno) VALUES('$pno')";
- }
+ $result=mysqli_query($conn,$qry);
+ $pno=mysqli_num_rows($result);
+ $pno=$pno+1;
+ 
+ $sql="INSERT INTO storie_no(sno) VALUES('$pno')";
+
 mysqli_query($conn,$sql);
  ?>
